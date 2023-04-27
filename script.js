faf270fe051e322a4177dc2f37dc39705419aca2
Denyse Uwumukiza <denysemukiza@gmail.com>
2023-04-27 14:48:05 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const navHamburger = document.getElementsByClassName('nav-hamburger')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+navHamburger.addEventListener('click', () => {
+    navbarLinks.ClassLists.toggle('active')
+})
+
