81f9e0f1a6bd3e46725bc1035f09d24ef31ce7b2
Denyse Uwumukiza <denysemukiza@gmail.com>
2023-05-23 17:33:09 +0300

    Corrected CRFL to LF in eslint

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".toggle-button").addEventListener("click", () => {
-    document
-      .querySelector(".navbar-links")
-      .classList.add("navbar-links-active");
-    document
-      .querySelector(".navbar-links")
-      .classList.remove("navbar-links-inactive");
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.toggle-button').addEventListener('click', () => {
+    document.querySelector('.navbar-links').classList.add('navbar-links-active');
+    document.querySelector('.navbar-links').classList.remove('navbar-links-inactive');
   });
-  document.querySelector(".close-button").addEventListener("click", () => {
-    document
-      .querySelector(".navbar-links")
-      .classList.remove("navbar-links-active");
-    document
-      .querySelector(".navbar-links")
-      .classList.add("navbar-links-inactive");
+  document.querySelector('.close-button').addEventListener('click', () => {
+    document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+    document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
   });
-  document.querySelector(".nav-link1").addEventListener("click", () => {
-    document
-      .querySelector(".navbar-links")
-      .classList.remove("navbar-links-active");
-    document
-      .querySelector(".navbar-links")
-      .classList.add("navbar-links-inactive");
+  document.querySelector('.nav-link1').addEventListener('click', () => {
+    document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+    document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
   });
-  document.querySelector(".nav-link2").addEventListener("click", () => {
-    document
-      .querySelector(".navbar-links")
-      .classList.remove("navbar-links-active");
-    document
-      .querySelector(".navbar-links")
-      .classList.add("navbar-links-inactive");
+  document.querySelector('.nav-link2').addEventListener('click', () => {
+    document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+    document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
   });
-  document.querySelector(".nav-link3").addEventListener("click", () => {
-    document
-      .querySelector(".navbar-links")
-      .classList.remove("navbar-links-active");
-    document
-      .querySelector(".navbar-links")
-      .classList.add("navbar-links-inactive");
+  document.querySelector('.nav-link3').addEventListener('click', () => {
+    document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+    document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
   });
 });
